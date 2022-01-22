74b1014030997717310e1e6d53d23a41da134378
Daniel Chong <temporaryaion1@hotmail.com>
2022-01-22 19:39:26 +0900

    make a guessing game

--- yt_tut/src/main.rs
+++ yt_tut/src/main.rs
@@ -121,4 +121,27 @@ fn main() {
     }
 
     println!("Find Best : {}", rand_string2.contains("Best"));
+
+    'outer: loop {
+        let number: i32 = 10;
+        println!("Pick a Number");
+
+        loop {
+            let mut line = String::new();
+            let input = stdin().read_line(&mut line);
+
+            let guess: Option<i32> = input.ok().map_or(None, |_| line.trim().parse().ok());
+
+            match guess {
+                None => println!("Enter a Number"),
+                Some(n) if n == number => {
+                    println!("You Guessed It!");
+                    break 'outer;
+                }
+                Some(n) if n < number => println!("Too Low"),
+                Some(n) if n > number => println!("Too High"),
+                Some(_) => println!("Error"),
+            }
+        }
+    }
 }
