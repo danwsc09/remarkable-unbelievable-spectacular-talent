5ba7063b2034a1951f3266f22ea1c0a7588407e7
Wonsang <sang.jeong@freedgrouptech.com>
2022-01-25 22:11:14 +0900

    start programming rust book

--- rust-lang-book/hello_cargo/src/main.rs
+++ rust-lang-book/hello_cargo/src/main.rs
@@ -1,5 +1,27 @@
 fn main() {
-    println!("Hello, world!!");
-    let x = 3;
-    print!("x is {}\n", x);
+    let a = 24;
+    let b = 36;
+    let result = gcd(a, b);
+    println!("Result: {}", result);
+    // let this_will_error = gcd(0, 5);
+    // println!("Error: {}", this_will_error);
+    let x: f32 = 1.57;
+    let y = {
+        println!("x is {}", x);
+        x.cos()
+    };
+    println!("y is {}", y);
+}
+
+fn gcd(mut m: u64, mut n: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    return n;
 }
