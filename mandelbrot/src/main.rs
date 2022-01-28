31e473d22d04003ef5ee6d545c0a9e3d54ad3696
Wonsang <sang.jeong@freedgrouptech.com>
2022-01-28 15:41:09 +0900

    chore: add comments for rustup documentation

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -3,6 +3,22 @@ use num::Complex;
 
 fn main() {
     println!("Hello, world!");
+    
+}
+
+/// Try to determine if 'c' is in the Mandelbrot set, using at most 'limit' iterations to decide.
+/// If 'c' is not a member, return 'Some(i)', where `i` is the number of iterations it took for `c`
+/// to leave the circle of radius two centered on the origin.
+/// If `c` seems to be a member, return `None`.
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+    None
 }
 
 #[allow(dead_code)]
