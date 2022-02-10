83459b1dd2c95d4d3ac47c719c9c90240e0f5d67
Wonsang <temporaryaion1@hotmail.com>
2022-02-10 21:02:22 +0900

    backtracking problem

--- ds/src/main.rs
+++ ds/src/main.rs
@@ -1,7 +1,9 @@
+mod b15469;
 mod l58;
-
 fn main() {
-    println!("Hello, world!");
-    let res = l58::length_of_last_word(" hello world\n".to_string());
-    assert_eq!(res, "world".len() as i32);
+    b15469::solve();
+    assert_eq!(
+        l58::length_of_last_word(" hello world\n".to_string()),
+        "world".len() as i32
+    );
 }
