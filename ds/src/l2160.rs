f0c9dd8096bc44e2d7962f6cd98583c86cde9afb
Wonsang <temporaryaion1@hotmail.com>
2022-02-21 08:08:04 +0900

    solve a couple puzzles

---
+++ ds/src/l2160.rs
@@ -0,0 +1,8 @@
+pub fn minimum_sum(num: i32) -> i32 {
+    let str = num.to_string();
+    let arr = Vec::new();
+    
+    // let arr: Vec<&str> = str.split("").collect();
+    println!("a: {:?}", arr);
+    0
+}
--- ds/src/main.rs
+++ ds/src/main.rs
@@ -1,9 +1,14 @@
 mod b15469;
 mod l58;
+mod l2161;
+mod l2160;
 fn main() {
-    b15469::solve();
-    assert_eq!(
-        l58::length_of_last_word(" hello world\n".to_string()),
-        "world".len() as i32
-    );
+    // b15469::solve();
+    // assert_eq!(
+    //     l58::length_of_last_word(" hello world\n".to_string()),
+    //     "world".len() as i32
+    // );
+    println!("running...");
+    let res = l2160::minimum_sum(2932);
+    println!("res:{:?}", res);
 }
