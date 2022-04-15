659ca99013c029c1c5f8393dc50ebbe7f8602c13
Wonsang <temporaryaion1@hotmail.com>
2022-04-15 17:54:59 +0900

    feat: implement random number guessing game

--- the-book/src/main.rs
+++ the-book/src/main.rs
@@ -1,83 +1,39 @@
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
+
 fn main() {
-    /*
-    let number = 6;
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by 2, 3, or 4!");
-    }
+    println!("Guess number");
+    let mut rng = rand::thread_rng();
+    let secret_number = rng.gen_range(1..101);
+    println!("Secret number: {}", secret_number);
+    
+    loop {
+        println!("Input your guess:");
 
-    let condition = true;
+        let mut guess = String::new();
 
-    let number = if condition { 5 } else { 6 };
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error while reading input");
 
-    println!("The value of number is: {}", number);
-    */
-    /*
-    let mut count = 0;
-    'counting_up: loop {
-        println!("count = {}", count);
-        let mut remaining = 10;
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input.");
+                continue
+            }
+        };
 
-        loop {
-            println!("remaining = {}", remaining);
-            if remaining == 9 {
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
                 break;
             }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
         }
-
-        count += 1;
-    }
-    println!("End count = {}", count);
-    */
-    let mut counter = 0;
-    let result = loop {
-        counter += 1;
-
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-
-    println!("The result is {}", result);
-
-    let a = [10, 20, 30, 40, 50];
-
-    for element in a {
-        println!("the value is {}", element);
-    }
-
-    for number in (1..4).rev() {
-        println!("{}!", number);
-    }
-    println!("liftoff!");
-
-    println!(
-        "100 deg F is {} deg C.",
-        convert_temperature("c", 100).unwrap()
-    );
-    println!(
-        "50 deg C is {} deg F.",
-        convert_temperature("f", 50).unwrap()
-    );
-    println!(
-        "50 deg C is {} deg T.",
-        convert_temperature("t", 50).unwrap()
-    );
-}
-
-fn convert_temperature(to: &str, degree: i32) -> Result<i32, std::io::Error> {
-    match to {
-        "c" => Ok((degree - 32) * 5 / 9),
-        "f" => Ok(degree * 9 / 5 + 32),
-        _ => panic!(),
     }
 }
